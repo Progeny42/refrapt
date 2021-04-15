619683a591bd20f1b08bb3af92d854802f08768b
Kyle Hill <48336180+Progeny42@users.noreply.github.com>
2021-04-15 23:53:21 +0100

    Significant Progress

--- refrapt/classes.py
+++ refrapt/classes.py
@@ -1,18 +1,40 @@
 from enum import Enum
 import logging
+import os
+import multiprocessing 
 
 class Settings:
     def __init__(self):
         self._architecture = "i386"
-        self._mirrorPath = "./mirror"
-        self._skelPath = "./skel"
-        self._varPath = "./var"
+
+        # TODO : Development only. Remove in production
+        if os.name == "nt":
+            self._mirrorPath = "K:/Working/refrapt/mirror"
+            self._skelPath = "K:/Working/refrapt/skel"
+            self._varPath = "K:/Working/refrapt/var"
+        else:
+            self._mirrorPath = "./mirror"
+            self._skelPath = "./skel"
+            self._varPath = "./var" 
+
+        self._contents = True
+        self._threads = multiprocessing.cpu_count()
+        self._authNoChallenge = False
+        self._noCheckCertficate = False
+        self._unlink = False
+        self._useProxy = False
+        self._httpProxy = None
+        self._httpsProxy = None
+        self._proxyUser = None
+        self._proxyPass = None
+        self._limitRate = "100m"
+        self._language = "en"
 
     def Parse(self, config):
         for line in config:
             if line.startswith('#'):
                 continue
-            if line.startswith("set"):
+            elif line.startswith("set"):
                 self.__ParseSetting(line)
 
     def __ParseSetting(self, setting):
@@ -40,15 +62,85 @@ class Settings:
     def VarPath(self) -> str:
         return self._varPath
 
+    @property
+    def Contents(self) -> bool:
+        return self._contents
+
+    @property
+    def Threads(self) -> int:
+        return self._threads
+        
+    @property
+    def AuthNoChallege(self) -> bool:
+        return self._authNoChallenge
+
+    @property
+    def NoCheckCertificate(self) -> bool:
+        return self._noCheckCertficate
+    
+    @property
+    def Unlink(self) -> bool:
+        return self._unlink
+
+    @property
+    def UseProxy(self) -> bool:
+        return self._useProxy
+
+    @property
+    def UseHttpProxy(self) -> bool:
+        return len(self._httpProxy) > 0
+
+    @property
+    def HttpProxy(self) -> str:
+        return self._httpProxy
+
+    @property
+    def UseHttpsProxy(self) -> bool:
+        return len(self._httpsProxy) > 0
+
+    @property
+    def HttpsProxy(self) -> str:
+        return self._httpsProxy
+
+    @property
+    def UseProxyUser(self) -> bool:
+        return len(self._proxyUser) > 0
+
+    @property
+    def ProxyUser(self) -> str:
+        return self._proxyUser
+
+    @property
+    def UseProxyPassword(self) -> bool:
+        return len(self._proxyPass) > 0
+
+    @property
+    def ProxyPassword(self) -> str:
+        return self._proxyPass
+
+    @property
+    def LimitRate(self) -> str:
+        return self._limitRate
+
+    @property
+    def Language(self) -> str:
+        return self._language
+
 
 class SourceType(Enum):
-    BIN  = 0
-    SRC  = 1
-    ARCH = 2
+    BIN = 0
+    SRC = 1
+
+class UrlType(Enum):
+    Index       = 0
+    Translation = 1
+    Dep11       = 2
+    Archive     = 3
 
 class Source:
-    def __init__(self, line):
+    def __init__(self, line, defaultArch):
         self._sourceType = SourceType.BIN
+        self._architectures = []
         self._uri = None
         self._distribution = None
         self._components = []
@@ -62,12 +154,28 @@ class Source:
             self._sourceType = SourceType.BIN
         elif 'deb-src' in elements[0]:
             self._sourceType = SourceType.SRC
-        else:
-            self._sourceType = SourceType.ARCH
 
-        self._uri           = elements[1]
-        self._distribution  = elements[2]
-        self._components    = elements[3:]
+        elementIndex = 1
+
+        # If Architecture(s) is specified, store it, else set the default
+        if "[" in line and "]" in line:
+            # Architecture is defined
+            archList = line.split("[")[1].split("]")[0].split("=")[1]
+            self._architectures = archList.split(",")
+            elementIndex += 1
+        else:
+            self._architectures.append(defaultArch)
+
+        self._uri           = elements[elementIndex]
+        self._distribution  = elements[elementIndex + 1]
+        self._components    = elements[elementIndex + 2:]
+
+        # logging.debug("Source")
+        # logging.debug(f"\tKind:         {self._sourceType}")
+        # logging.debug(f"\tArch:         {self._architectures}")
+        # logging.debug(f"\tUri:          {self._uri}")
+        # logging.debug(f"\tDistribution: {self._distribution}")
+        # logging.debug(f"\tComponents:   {self._components}")
 
     @property
     def SourceType(self) -> SourceType:
@@ -84,4 +192,8 @@ class Source:
     @property   
     def Components(self) -> list:
         return self._components
+
+    @property   
+    def Architectures(self) -> list:
+        return self._architectures
 
--- refrapt/refrapt.py
+++ refrapt/refrapt.py
@@ -1,16 +1,28 @@
 import sys
 import logging
 import click
+import os
+import time
 from pathlib import Path
+import multiprocessing 
+import re
+import math
+import codecs
 
-import classes
+from classes import (
+    Settings,
+    Source,
+    SourceType,
+    UrlType
+)
 
 # Consider these as Constants
 CONFIG_FILE = "refrapt.cfg"
 
-settings = classes.Settings()
+settings = Settings()
 configData = []
 urlDownloads = []
+sources = []
 
 @click.command()
 def main():
@@ -34,20 +46,46 @@ def main():
     Path(settings.SkelPath).mkdir(parents=True, exist_ok=True)
     Path(settings.VarPath).mkdir(parents=True, exist_ok=True)
 
-    print(settings.Architecture)
-
-    ProcessSources()
-
-@click.command()
-def ProcessSources():
-    sources = []
-
+    firstSourceLineIndex = 0
     for line in configData:
-        sources.append(classes.Source(line))
+        if len(line) > 0 and line.startswith('#') or line.startswith("set"):
+            firstSourceLineIndex += 1
+            continue
+        else:
+            break
+
+    sourceList = [x for x in configData[firstSourceLineIndex:] if not x.startswith("#")]
+
+
+    ProcessSources(sourceList)
+    os.chdir(settings.SkelPath)
+    # DownloadUrls(UrlType.Index)
+    
+    # ProcessTranslationIndexes()
+    # DownloadUrls(UrlType.Translation)
+
+    # ProcessDep11Files()
+    # DownloadUrls(UrlType.Dep11)
+
+    # Main Downloads
+    # Path(settings.VarPath + "/ALL").mkdir(parents=True, exist_ok=True)
+    # Path(settings.VarPath + "/NEW").mkdir(parents=True, exist_ok=True)
+    # Path(settings.VarPath + "/MD5").mkdir(parents=True, exist_ok=True)
+    # Path(settings.VarPath + "/SHA1").mkdir(parents=True, exist_ok=True)
+    # Path(settings.VarPath + "/SHA256").mkdir(parents=True, exist_ok=True)
+
+    ProcessIndexes()
+
+#@click.command()
+def ProcessSources(sourceList):
+    global sources
+    global urlDownloads
+
+    for line in sourceList:
+        sources.append(Source(line, settings.Architecture))
 
     for source in sources:
-        if source.SourceType == classes.SourceType.SRC:
-            logging.debug("Processing SRC")
+        if source.SourceType == SourceType.SRC:
 
             if len(source.Components) > 0:
                 url = source.Uri + "/dists/" + source.Distribution + "/"
@@ -67,9 +105,329 @@ def ProcessSources():
                 urlDownloads.append(source.Uri + "/" + source.Distribution + "/Sources.gz")
                 urlDownloads.append(source.Uri + "/" + source.Distribution + "/Sources.bz2")
                 urlDownloads.append(source.Uri + "/" + source.Distribution + "/Sources.xz")
-        elif source.SourceType == classes.SourceType.BIN:
-            logging.debug("Processing BIN")
+        elif source.SourceType == SourceType.BIN:
 
+            if len(source.Components) > 0:
+                url = source.Uri + "/dists/" + source.Distribution + "/"
+
+                urlDownloads.append(url + "InRelease")
+                urlDownloads.append(url + "Release")
+                urlDownloads.append(url + "Release.gpg")
+
+                if settings.Contents:
+                    for architecture in source.Architectures:
+                        urlDownloads.append(url + "Contents-" + architecture + ".gz")
+                        urlDownloads.append(url + "Contents-" + architecture + ".bz2")
+                        urlDownloads.append(url + "Contents-" + architecture + ".xz")
+
+                for component in source.Components:
+                    for architecture in source.Architectures:
+                        if settings.Contents:
+                            urlDownloads.append(url + component + "/Contents-" + architecture + ".gz")
+                            urlDownloads.append(url + component + "/Contents-" + architecture + ".bz2")
+                            urlDownloads.append(url + component + "/Contents-" + architecture + ".xz")
+
+                        urlDownloads.append(url + component + "/binary-" + architecture + "/Release")
+                        urlDownloads.append(url + component + "/binary-" + architecture + "/Packages.gz")
+                        urlDownloads.append(url + component + "/binary-" + architecture + "/Packages.bz2")
+                        urlDownloads.append(url + component + "/binary-" + architecture + "/Packages.xz")
+                        
+                    urlDownloads.append(url + component + "/i18n/Index")
+            else:
+                urlDownloads.append(source.Uri + "/" + source.Distribution + "/Release")
+                urlDownloads.append(source.Uri + "/" + source.Distribution + "/Release.gpg")
+                urlDownloads.append(source.Uri + "/" + source.Distribution + "/Sources.gz")
+                urlDownloads.append(source.Uri + "/" + source.Distribution + "/Sources.bz2")
+                urlDownloads.append(source.Uri + "/" + source.Distribution + "/Sources.xz") 
+
+#@click.command()
+#@click.argument("kind", type=UrlType)
+def DownloadUrls(kind):
+    if len(urlDownloads) == 0:
+        logging.debug("No files to download")
+        return
+
+    threadCount = settings.Threads
+
+    # for url in urlDownloads:
+    #     print(url)   
+
+    if len(urlDownloads) < threadCount:
+        threadCount = len(urlDownloads)
+
+    arguments = []
+
+    if settings.AuthNoChallege:
+        arguments.append("--auth-no-challenge")
+    if settings.NoCheckCertificate:
+        arguments.append("--no-check-certificate")
+    if settings.Unlink:
+        arguments.append("--unlink")
+
+    if settings.UseProxy:
+        arguments.append("-e use_proxy=yes")
+
+        if settings.UseHttpProxy:
+            arguments.append("-e http_proxy=" + settings.HttpProxy)
+        if settings.UseHttpsProxy:
+            arguments.append("-e https_proxy=" + settings.HttpsProxy)
+        if settings.UseProxyUser:
+            arguments.append("-e proxy_user=" + settings.ProxyUser)
+        if settings.UseProxyPassword:
+            arguments.append("-e proxy_password=" + settings.ProxyPassword)
+
+    logging.info(f"Downloading {len(urlDownloads)} {kind.name} files using {threadCount} threads...\n")
+    logging.debug(f"args: {arguments}")
+
+    i = 0
+    processes = []
+
+    chunkSize = math.ceil(len(urlDownloads) / threadCount)
+
+    while len(urlDownloads) > 0:
+        threadUrls = urlDownloads[0:chunkSize]
+        del urlDownloads[0:chunkSize]
+
+        with open (settings.VarPath + f"/{kind.name}-urls.{i}", "w") as f:
+            for url in threadUrls:
+                f.write(url + "\n")
+
+        p = multiprocessing.Process(target=DownloadUrlsProcess, args=(threadUrls, kind.name, i, arguments))
+        p.daemon = True
+        p.start()
+
+        processes.append(p)
+
+        i += 1
+
+    logging.info(f"Begin time: " + time.strftime("%H:%M:%S", time.localtime()) + " \n[" + str(len(processes)) + "]...")
+    for process in processes:
+        process.join()
+        logging.info(f"[" + str(len(processes) - i) + "]...")
+        i -= 1
+    logging.info(f"End time:" + time.strftime("%H:%M:%S", time.localtime()) + "\n\n")
+
+def DownloadUrlsProcess(urls, kind, index, args):
+    baseCommand   = "wget --no-cache -N"
+    rateLimit     = f"--limit-rate={settings.LimitRate}"
+    retries       = "-t 5"
+    recursiveOpts = "-r -l inf"
+    logFile       = f"-o {settings.VarPath}/{kind}-log.{index}"
+    inputFile     = f"-i {settings.VarPath}/{kind}-urls.{index}"
+
+    #print(f"{baseCommand} {rateLimit} {retries} {recursiveOpts} {logFile} {inputFile} {args}")
+    #os.system(f"{baseCommand} {rateLimit} {retries} {recursiveOpts} {logFile} {inputFile} {args}")
+
+def ProcessTranslationIndexes():
+    logging.debug("Processing translation indexes: [")
+
+    for source in sources:
+        if source.SourceType != SourceType.BIN:
+            continue
+        #logging.debug("T")
+        if len(source.Components) > 0:
+            url = source.Uri + "/dists/" + source.Distribution + "/"
+
+            for component in source.Components:
+                ProcessTranslationIndex(url, component)
+
+    logging.debug("]\n\n")
+
+
+def ProcessTranslationIndex(url, component):
+    # Extract all translation files from the dists/$DIST/$COMPONENT/i18n/Index
+    # file. Fall back to parsing dists/$DIST/Release if i18n/Index is not found.
+
+    #distUri = RemoveDoubleSlashes(url)
+    baseUri = url + component + "/i18n/"
+    indexUri = baseUri + "Index"
+    indexPath = settings.SkelPath + "/" + SanitiseUri(indexUri)
+
+    logging.debug(indexPath)
+
+    checksums = False
+
+    if os.path.isfile(indexPath):
+        with open (indexPath) as f:
+            for line in f:
+                if checksums:
+                    if re.search("^ +(.*)$", line):
+                        parts = list(filter(None, line.split(" ")))
+
+                        # parts[0] = sha1
+                        # parts[1] = size
+                        # parts[2] = filename
+
+                        if len(parts) == 3:
+                            #if  "-" + settings.Language in parts[2]:
+                            urlDownloads.append(baseUri + parts[2])
+                        else:
+                            logging.warn(f"Malformed checksum line '{line}' in {indexUri}")
+                    else:
+                        checksums = False
+                else:
+                    checksums = "SHA256:" in line or "SHA1:" in line or "MD5Sum:" in line
+    else:
+        FindTranslationFilesInRelease(url, component)
+        return
+
+def FindTranslationFilesInRelease(uri, component):
+    # Look in the dists/$DIST/Release file for the translation files that belong
+    # to the given component.
+
+    releaseUri = uri + "Release"
+    releasePath = settings.SkelPath + "/" +  SanitiseUri(releaseUri)
+
+    checksums = False
+
+    with open (releasePath) as f:
+        for line in f:
+            if checksums:
+                if re.search("^ +(.*)$", line):
+                    parts = list(filter(None, line.split(" ")))
+
+                    # parts[0] = sha1
+                    # parts[1] = size
+                    # parts[2] = filename
+
+                    if len(parts) == 3:
+                        #if re.match(f"^{component}/i18n/Translation-{settings.Language}*\.bz2$", parts[2]):
+                        if re.match(f"^{component}/i18n/Translation-[^./]*\.bz2$", parts[2]):
+                            urlDownloads.append(uri + parts[2].rstrip())
+                    else:
+                        logging.warn(f"Malformed checksum line '{line}' in {releaseUri}")
+                else:
+                    checksums = False
+            else:
+                checksums = "SHA256:" in line or "SHA1:" in line or "MD5Sum:" in line
+
+def ProcessDep11Files():
+    logging.debug("Processing DEP-11 indexes: [")
+
+    for source in sources:
+        if source.SourceType != SourceType.BIN:
+            continue
+        #logging.debug("D")
+        if len(source.Components) > 0:
+            url = source.Uri + "/dists/" + source.Distribution + "/"
+
+            for component in source.Components:
+                FindDep11FilesInRelease(url, component, source.Architectures)
+
+    logging.debug("]\n\n")
+
+def FindDep11FilesInRelease(uri, component, architectures):
+    # Look in the dists/$DIST/Release file for the DEP-11 files that belong
+    # to the given component and architecture.
+
+    releaseUri = uri + "Release"
+    releasePath = settings.SkelPath + "/" +  SanitiseUri(releaseUri)
+
+    checksums = False
+
+    with open (releasePath) as f:
+        for line in f:
+            if checksums:
+                if re.search("^ +(.*)$", line):
+                    parts = list(filter(None, line.split(" ")))
+
+                    # parts[0] = sha1
+                    # parts[1] = size
+                    # parts[2] = filename
+
+                    if len(parts) == 3:
+                        for arch in architectures:
+                            if re.match(f"^{component}/dep11/(Components-{arch}\.yml|icons-[^./]+\.tar)\.(gz|bz2|xz)$", parts[2]):
+                                urlDownloads.append(uri + parts[2].rstrip())
+                    else:
+                        logging.warn(f"Malformed checksum line '{line}' in {releaseUri}")
+                else:
+                    checksums = False
+            else:
+                checksums = "SHA256:" in line or "SHA1:" in line or "MD5Sum:" in line
+
+def ProcessIndexes():
+    logging.debug("Processing indexes: [")
+
+    for source in sources:
+        
+        if source.SourceType == SourceType.SRC:
+            #logging.debug("S")
+            if len(source.Components) > 0:
+                for component in source.Components:
+                    ProcessIndex(source.Uri, f"/dists/{source.Distribution}/{component}/source/Sources")
+            else:
+                ProcessIndex(source.Uri, f"/{source.Distribution}/Sources")
+        elif source.SourceType == SourceType.BIN:
+            #logging.debug("P")
+            if len(source.Components) > 0:
+                for arch in source.Architectures:
+                    for component in source.Components:
+                        ProcessIndex(source.Uri, f"/dists/{source.Distribution}/{component}/binary-{arch}/Packages")
+            else:
+                ProcessIndex(source.Uri, f"/{source.Distribution}/Packages")
+
+    logging.debug("]\n\n")
+
+def ProcessIndex(uri, index):
+    path = SanitiseUri(uri)
+
+    mirror = settings.MirrorPath + "/" + path
+
+    if os.path.isfile(f"{path}/{index}.gz"):
+        os.system(f"gunzip < {path}/{index}.gz > {path}/{index}")
+    elif os.path.isfile(f"{path}/{index}.xz"):
+        os.system(f"xz -d {path}/{index}.xz > {path}/{index}")
+    elif os.path.isfile(f"{path}/{index}.bz2"):
+        os.system(f"bzip2 -d {path}/{index}.bz2 > {path}/{index}")
+
+    package = []
+    lines = []
+
+    with open (settings.VarPath + "/ALL", "a+") as allFile, \
+         open (settings.VarPath + "/NEW", "a+") as newFile, \
+         open (settings.VarPath + "/MD5", "a+") as md5File, \
+         open (settings.VarPath + "/SHA1", "a+") as sha1File, \
+         open (settings.VarPath + "/SHA256", "a+") as sha256File:
+        with open (f"{path}/{index}", "rb") as f:
+            package = f.readlines()
+            #l = re.compile("^([\w\-]+:)").split(f.read().decode())
+
+        for line in package:
+            lines.append(line.decode().rstrip())
+
+        package = lines
+
+        if any("Filename:" in s for s in package):
+            # Packages Index
+            WriteAllLines("Filename:", package, allFile, path)
+            # WriteAllLines("MD5sum:", package, md5File, path)
+            # WriteAllLines("SHA1:", package, sha1File, path)
+            # WriteAllLines("SHA256:", package, sha1File, path)
+
+
+def WriteAllLines(identifier, package, file, path):
+    results = [i for i in package if i.startswith(identifier)]
+
+    for result in results:
+        splitLine = re.sub("^([\w\-]+:)", "", result).strip()
+        file.write(f"{path}/{splitLine}\n")
+
+def RemoveDoubleSlashes(string) -> str:
+    string = string.replace("\\\\", "\\")
+    string = string.replace("//", "/")
+
+    return string
+
+def SanitiseUri(uri) -> str:
+    uri = re.sub("^(\w+)://", "", uri)
+
+    if '@' in uri:
+        uri = re.sub("^([^@]+)?@?/", "", uri)
+
+    uri = re.sub(":\d+", "", uri) # Port information
+   
+    return uri
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)

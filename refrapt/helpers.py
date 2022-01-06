4f365de38111fa70ad4831c797349ade69f91feb
Kyle Hill <48336180+Progeny42@users.noreply.github.com>
2022-01-06 22:00:24 +0000

    Update helpers.py
    Comment updates only

--- refrapt/helpers.py
+++ refrapt/helpers.py
@@ -4,7 +4,7 @@ import re
 
 def SanitiseUri(uri: str) -> str:
     """Sanitise a Uri so it is suitable for filesystem use."""
-    uri = re.sub(r"^(\w+)://", "", uri)
-    uri = re.sub(r":\d+", "", uri) # Port information
+    uri = re.sub(r"^(\w+)://", "", uri) # Protocol
+    uri = re.sub(r":\d+", "", uri) # Port
 
     return uri

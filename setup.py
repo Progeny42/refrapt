2028ede176c6ee72f28d32b9c4234f37f6845ba4
Kyle Hill <48336180+Progeny42@users.noreply.github.com>
2023-10-04 22:06:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='Refrapt',
-    version='0.4.6',
+    version='0.4.7',
     description='A tool to mirror Debian repositories for use as a local mirror.',
     python_requires='>=3.9',
     long_description=README,
